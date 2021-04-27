15533f63378ae98e50612e752387ed6a760bbc66
Simran Priya <simranmadhupriya@gmail.com>
2021-04-27 21:03:44 +0530

    commit

--- maths.py
+++ maths.py
@@ -1,9 +1,19 @@
 def add_ten(x):
     if type(x) == str:
-        x_num = int(x)
-        result_num = x_num + 10
-        return str(result_num)
+        number = int(x)
+        result = number + 10
+        return str(result)
     elif x is None:
         return None
     else:
         return x + 10
+
+def subtract_ten(x):
+    if type(x) == str:
+        number = int(x)
+        result = number - 10
+        return str(result)
+    elif x is None:
+        return None
+    else:
+        return x - 10
--- test.py
+++ test.py
@@ -4,18 +4,27 @@ path_large_number_fixture = 'large_num_fixture.txt'
 
 
 def test_add_ten():
-    """ Tests the add_ten function """
     assert 15 == maths.add_ten(5)
 
 
 def test_add_ten_str_int():
-    """ Tests the add_ten function for string input """
     assert maths.add_ten('5') == '15'
 
 
 def test_add_ten_none():
-    """ Tests that add_ten throws a ValueError when None input given """
     assert maths.add_ten(None) is None
+
+def test_subtract_ten():
+    assert 25 == maths.add_ten(15)
+
+
+def test_subtract_ten_str_int():
+    assert maths.add_ten('15') == '25'
+
+
+def test_subtract_ten_none():
+    assert maths.subtract_ten(None) is None
+
 
 
 def test_large_number():
